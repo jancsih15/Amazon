baa11c171c7d759c916fb0b134566636a7169536
Bonhart67 <lozsek.peter@gmail.com>
2019-04-29 20:55:55 +0200

    jancsinak unit test gyakorlás

---
+++ Amazon/Assets/Scripts/Entity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Entity {
+
+    private int posX, posY;
+
+    public Entity(int x, int y) {
+        try {
+            if (CheckCoords(x, y)) throw new System.Exception();
+            this.posX = x;
+            this.posY = y;
+        } catch (System.Exception) {
+            //Debug.LogError("Invalid coordinate on Entity generation!");
+        }
+    }
+
+    private bool CheckCoords(int x, int y) {
+        return x < 0 || x > 7 || y < 0 || y > 0;
+    }
+}
---
+++ Amazon/Assets/Test/Editor/EntityTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityTests {
+
+    public bool ThrowsExceptionOnConstruction(int x, int y) {
+        try {
+            new Entity(x, y);
+            return false;
+        } catch (System.Exception) {
+            return true;
+        }
+    }
+
+    [Test]
+    public void EntityConstructTest() {
+        Assert.False(ThrowsExceptionOnConstruction(0, 0));
+        Assert.False(ThrowsExceptionOnConstruction(7, 7));
+        Assert.True(ThrowsExceptionOnConstruction(12, 0));
+        Assert.True(ThrowsExceptionOnConstruction(3, 53));
+        Assert.True(ThrowsExceptionOnConstruction(12, 53));
+        Assert.True(ThrowsExceptionOnConstruction(4, -20));
+    }
+}

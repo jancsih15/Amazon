868ccb75bc07f11a872b2f228d6f6e79c8b46270
Bonhart67 <lozsek.peter@gmail.com>
2019-04-29 20:09:09 +0200

    basic test for the noobs added

---
+++ Amazon/Assets/Scripts/Tests/UnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests {
+    public class UnitTests {
+        [Test]
+        public void TestFigurePosition() {
+            Assert.IsTrue(true);
+        }
+    }
+}
